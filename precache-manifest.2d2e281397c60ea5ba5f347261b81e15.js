d94541fbd5dcfee70d8309cb09c7a7cb2f31cac7
Matias <floresmatias0@gmail.com>
2021-05-31 16:36:10 -0300

    Updates

---
+++ precache-manifest.2d2e281397c60ea5ba5f347261b81e15.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c8a6a800c4a87d8b9a5fd1be8a2929",
+    "url": "/rick-morty/index.html"
+  },
+  {
+    "revision": "928ed25e58368a71717e",
+    "url": "/rick-morty/static/css/2.9a6475f2.chunk.css"
+  },
+  {
+    "revision": "0a078f512bc2abd17b3c",
+    "url": "/rick-morty/static/css/main.18a9be5a.chunk.css"
+  },
+  {
+    "revision": "928ed25e58368a71717e",
+    "url": "/rick-morty/static/js/2.7a2887b8.chunk.js"
+  },
+  {
+    "revision": "0bd2f0ff8e4a34e54e21193be9994472",
+    "url": "/rick-morty/static/js/2.7a2887b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a078f512bc2abd17b3c",
+    "url": "/rick-morty/static/js/main.26b9e862.chunk.js"
+  },
+  {
+    "revision": "3170e6637e90bf04d80f",
+    "url": "/rick-morty/static/js/runtime-main.65ad5b5d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/rick-morty/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/rick-morty/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/rick-morty/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/rick-morty/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/rick-morty/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "321c9524ca60e40d43cad7d15e6cb2d2",
+    "url": "/rick-morty/static/media/spinner.321c9524.gif"
+  }
+]);
